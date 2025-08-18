e27297761fe4ef27824979fa94f83cf8f884c1fa
Ahmed Mohamed <ahmedysatti@gmail.com>
2025-08-18 10:46:22 +0100

    HUNTER-26 in the process of configuring AWS Cognito resource and IdPs using CDK

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,59 @@
 import * as cdk from 'aws-cdk-lib';
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkStack extends cdk.Stack {
+export class HunterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const secret = new secretsmanager.Secret(this, "SecretsManager", {
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({}),
+        generateStringKey: "CognitoClientSecret"
+      }
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const userPool = new cognito.UserPool(this, "hunter-userpool", {
+      userPoolName: "hunter-users",
+      userVerification: {
+        emailStyle: cognito.VerificationEmailStyle.LINK,
+        emailSubject: "Verify your new account with Hunter",
+        emailBody: "Thank you for creating an account with Hunter. Please click {##here##} to verify your new account."
+      },
+      signInAliases: {
+        email: true
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      passwordPolicy: {
+        minLength: 12,
+        passwordHistorySize: 1,
+        requireDigits: true,
+        requireLowercase: true,
+        requireSymbols: true,
+        requireUppercase: true
+      },
+      signInPolicy: { allowedFirstAuthFactors: { password: true } }
+    });
+
+    new cognito.UserPoolIdentityProviderGoogle(this, "Google", {
+      clientId: "google-client-id",
+      clientSecretValue: secret.secretValueFromJson("CognitoClientSecret"),
+      userPool: userPool
+    })
+
+    new cognito.UserPoolIdentityProviderFacebook(this, "Facebook", {
+      clientId: "facebook-client-id",
+      clientSecret: secret.secretValueFromJson("CognitoClientSecret").toString(),
+      userPool: userPool
+    })
+
+    new cognito.UserPoolIdentityProviderApple(this, "Apple", {
+      clientId: "apple-client-id",
+      userPool: userPool,
+      keyId: "keyId",
+      teamId: "teamId",
+      privateKeyValue: secret.secretValueFromJson("CognitoClientSecret")
+    })
   }
 }
--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -1,17 +1,61 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Cdk from '../lib/cdk-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { HunterStack } from '../lib/cdk-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Cdk.CdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+const app = new cdk.App();
+const stack = new HunterStack(app, "HunterStack");
+const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
-});
+describe("Hunter user pool tests", () => {
+  test('Cognito user pool created', () => {
+    template.resourceCountIs("AWS::Cognito::UserPool", 1);
+  });
+
+  test("Cognito user pool resource should have the correct configured properties", () => {
+    template.hasResourceProperties('AWS::Cognito::UserPool', {
+      VerificationMessageTemplate: {
+        DefaultEmailOption: "CONFIRM_WITH_LINK",
+        EmailMessageByLink: "Thank you for creating an account with Hunter. Please click {##here##} to verify your new account.",
+        EmailSubjectByLink: "Verify your new account with Hunter"
+      },
+      UserPoolName: "hunter-users",
+      AccountRecoverySetting: {
+        RecoveryMechanisms: [{
+          Name: "verified_email",
+          Priority: 1
+        }]
+      },
+      Policies: {
+        PasswordPolicy: {
+          MinimumLength: 12,
+          RequireLowercase: true,
+          RequireNumbers: true,
+          RequireSymbols: true,
+          RequireUppercase: true,
+          PasswordHistorySize: 1
+        },
+        SignInPolicy: {
+          AllowedFirstAuthFactors: ["PASSWORD"]
+        }
+      }
+    });
+  })
+})
+
+describe("User pool identity providers tests", () => {
+  test('A Google identity provider should be created for hunter-users user pool', () => {
+    template.hasResourceProperties("AWS::Cognito::UserPoolIdentityProvider", {
+      ProviderType: "Google",
+    })
+  })
+  test('A Facebook identity provider should be created for hunter-users user pool', () => {
+    template.hasResourceProperties("AWS::Cognito::UserPoolIdentityProvider", {
+      ProviderType: "Facebook",
+    })
+  })
+  test('A Apple identity provider should be created for hunter-users user pool', () => {
+    template.hasResourceProperties("AWS::Cognito::UserPoolIdentityProvider", {
+      ProviderType: "SignInWithApple",
+    })
+  })
+})
