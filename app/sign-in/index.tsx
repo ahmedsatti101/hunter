cfbbf3953e9d2261c6f2bc0b3bedf442ae7c00c6
Ahmed Mohamed <ahmedysatti@gmail.com>
2025-08-27 12:08:43 +0100

    HUNTER-8 Created and styled the sign in with email screen. I need to write tests for this issue/ticket before pushing to main.

--- app/sign-in/index.tsx
+++ app/sign-in/index.tsx
@@ -2,21 +2,113 @@ import { Stack } from "expo-router";
 import { Text, View } from "react-native";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
+import { object, string, ObjectSchema } from "yup";
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Label } from "~/components/ui/label";
+
+interface UserSignIn {
+  email: string;
+  password: string;
+}
+
+const createYupSchema = <T extends object>(schema: ObjectSchema<T>): ObjectSchema<T> => schema;
+
+const userFormSchema = createYupSchema<UserSignIn>(
+  object().shape({
+    email: string()
+      .required("Email is required")
+      .email("Invalid email format"),
+    password: string()
+      .required("Password is required")
+      .min(12, "Password length must at least be 12 characters")
+      .max(256, "Password maximum length exceeded")
+  })
+);
 
 export default function SignInWithEmail() {
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(userFormSchema),
+    defaultValues: {
+      email: "",
+      password: ""
+    }
+  });
+
+  const mediumFont = "WorkSans-Medium";
+  const boldFont = "WorkSans-Bold";
+
+  const PASSREQS = [
+    "Minimum of 12 characters",
+    "Must contain at least one digit",
+    "Must contain a lowercase character",
+    "Must contain a uppercase character",
+    "Must contain a symbol"
+  ];
+
+  const submitToCognito = (formData: UserSignIn) => {
+    console.log(formData);
+  };
+
   return (
-    <View className="flex-1 justify-center align-center">
-      <Stack.Screen options={{ headerShown: false }} />
-      <Text>Email</Text>
-      <Input keyboardType="email-address" textContentType="emailAddress" autoComplete="email" placeholder="john.doe@example.com" />
-      <Text>Password</Text>
-      <Input keyboardType="visible-password" textContentType="password" />
-      <Button className="bg-[#000]">
-        <Text className="bg-[#fff]">Sign in</Text>
-      </Button>
-      <Button>
-        <Text className="underline text-[#4160de]">Forgot password?</Text>
+    <View className="flex-1 justify-center items-center m-2">
+      <Stack.Screen options={{ headerTitle: "", headerRight: undefined }} />
+
+      <Text className="text-3xl" style={{ fontFamily: boldFont }}>Sign in</Text>
+
+      <View className="m-2">
+        <Label style={{ fontFamily: mediumFont, fontWeight: "bold" }} htmlFor="email" nativeID='email'>Email</Label>
+        <Controller
+          control={control}
+          rules={{ required: true }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              value={value}
+              onChangeText={onChange}
+              placeholder="john.doe@example.com"
+              keyboardType="email-address"
+              textContentType="emailAddress"
+              autoComplete="off"
+              style={{ fontFamily: mediumFont }}
+              className="p-2 border-[#a7a7a7] rounded-lg text-xl h-[50px] w-[300px]"
+            />
+          )}
+          name="email"
+        />
+        {errors.email && <Text className="text-red-700" style={{ fontFamily: mediumFont }}>{errors.email.message}</Text>}
+      </View>
+
+      <View className="m-2">
+        <Label style={{ fontFamily: mediumFont, fontWeight: "bold" }} htmlFor="password" nativeID='password'>Password</Label>
+        <Controller
+          control={control}
+          rules={{ required: true }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              textContentType="password"
+              secureTextEntry
+              value={value}
+              onChangeText={onChange}
+              className="p-2 border-[#a7a7a7] rounded-lg text-xl h-[50px] w-[300px]"
+            />
+          )}
+          name="password"
+        />
+        {errors.password && <Text className="text-red-700" style={{ fontFamily: mediumFont }}>{errors.password.message}</Text>}
+
+        <Text style={{ fontFamily: boldFont }} className="text-md mt-2 text-[#7b7b7b]">Password requirements:</Text>
+        {PASSREQS.map((req, idx) => {
+          return (
+            <Text key={idx} style={{ fontFamily: boldFont }} className="text-md text-[#7b7b7b]">{req}</Text>
+          )
+        })}
+        <Button className="justify-start mt-3">
+          <Text className="text-base underline text-[#4160de]" style={{ fontFamily: mediumFont }}>Forgot password?</Text>
+        </Button>
+      </View>
+      <Button className="bg-[#000] ml-60 mt-3" onPress={handleSubmit(submitToCognito)}>
+        <Text className="text-white border rounded-md p-4" style={{ fontFamily: mediumFont }}>Sign in</Text>
       </Button>
     </View>
   );
-}
+};
