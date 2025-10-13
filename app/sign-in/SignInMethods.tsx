f8a06d20dee2f76a043f03c26e49a7b8dd523bbb
Ahmed Mohamed <ahmedysatti@gmail.com>
2025-10-13 12:17:06 +0100

    Attached a function that attempts to sign in user with Google. Further work needs to be done to ensure the sign in functionality functions as intended.

--- app/sign-in/SignInMethods.tsx
+++ app/sign-in/SignInMethods.tsx
@@ -1,15 +1,60 @@
-import { View } from "react-native";
+import { Platform, View } from "react-native";
 import { Button } from '~/components/ui/button';
 import { Text } from '~/components/ui/text';
 import { AntDesign, FontAwesome5, Feather } from '@expo/vector-icons';
 import { Stack, useRouter } from "expo-router";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "~/context/ThemeContext";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import { GoogleSignin, isSuccessResponse, statusCodes } from "@react-native-google-signin/google-signin"; //https://react-native-google-signin.github.io/docs/original
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function SignInMethods() {
   const router = useRouter();
   const buttonStyle = "p-3 m-3 bg-[#fbfbfb] rounded-lg flex-row border border-[#a7a7a7]";
   const { darkMode } = useContext(ThemeContext);
+  const [req, res, prompt] = Google.useAuthRequest({
+    webClientId: "",
+    androidClientId: ""
+  });
+  const [user, setUser] = useState<any | null>(null);
+
+  console.log(req, "<<< Google req");
+  console.log(res, "<<< Google res");
+
+  GoogleSignin.configure()
+
+  const handleGoogleSignin = async () => {
+    if (Platform.OS === "web") {
+      await prompt();
+    } else {
+      await GoogleSignin.hasPlayServices();
+
+      try {
+        const response = await GoogleSignin.signIn();
+        if (isSuccessResponse(response)) {
+          console.log(response.data);
+          setUser(response.data)
+        }
+      } catch (error: any) {
+        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+          // user cancelled the login flow
+          console.log('User cancelled the login flow');
+        } else if (error.code === statusCodes.IN_PROGRESS) {
+          // operation (e.g. sign in) is in progress already
+          console.log('Sign in is in progress');
+        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+          // play services not available or outdated
+          console.log('Play services not available');
+        } else {
+          // some other error happened
+          console.error(error);
+        }
+      }
+    }
+  }
 
   return (
     <View className="flex-1 justify-center items-center" style={{ backgroundColor: `${darkMode === true ? '#1b1b1b' : '#fff'}` }}>
@@ -21,10 +66,11 @@ export default function SignInMethods() {
           <Feather name="mail" size={24} color="black" className="m-2" />
           <Text className="text-lg" style={{ fontFamily: 'WorkSans-Medium' }}>Sign in with email</Text>
         </Button>
-        <Button variant="outline" className={buttonStyle}>
+        <Button variant="outline" className={buttonStyle} onPress={handleGoogleSignin}>
           <AntDesign name="google" size={24} color="black" className="m-2" />
           <Text className="text-lg" style={{ fontFamily: 'WorkSans-Medium' }}>Sign in with Google</Text>
         </Button>
+
         <Button variant="outline" className={buttonStyle}>
           <FontAwesome5 name="facebook-f" size={24} color="black" className="m-2" />
           <Text className="text-lg" style={{ fontFamily: 'WorkSans-Medium' }}>Sign in with Facebook</Text>
