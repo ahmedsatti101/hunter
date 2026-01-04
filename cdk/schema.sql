29595968b2dd9425af26b4d4044143872bfe730e
Ahmed Mohamed <ahmedysatti@gmail.com>
2026-01-04 12:30:22 +0000

    Created SQL file that creates the database and its tables.

---
+++ cdk/schema.sql
@@ -0,0 +1,38 @@
+/*Do i need a trigger?*/
+
+DROP TABLE IF EXISTS screenshots;
+DROP TABLE IF EXISTS entries;
+
+DROP TYPE IF EXISTS status_type;
+DROP EXTENSION IF EXISTS pgcrypto;
+
+DROP DATABASE IF EXISTS hunter;
+
+CREATE DATABASE hunter;
+
+\c hunter
+
+CREATE EXTENSION pgcrypto;
+
+CREATE TYPE status_type AS ENUM ('Applied', 'Successful', 'Unsuccessful', 'Going for interview', 'Declined offer', 'Role offered');
+
+CREATE TABLE entries (
+  id     UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  title   varchar(50) NOT NULL,
+  description   varchar(350) NOT NULL,
+  employer    varchar(50) NOT NULL,
+  contact   varchar(50),
+  status  status_type NOT NULL,
+  submission_date   date NOT NULL,
+  last_updated    timestamp DEFAULT CURRENT_TIMESTAMP,
+  location    varchar(100),
+  notes   varchar(350),
+  found_where   varchar(100) NOT NULL
+);
+
+CREATE TABLE screenshots(
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  entry_id UUID references entries(id),
+  uploaded_at timestamp DEFAULT CURRENT_TIMESTAMP,
+  url text NOT NULL
+);
